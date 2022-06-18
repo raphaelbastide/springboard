37d8d89887508fba3e2c1308f38a999015a3edf2
Stef Dunlap <stef@kindrobot.ca>
2022-06-18 10:25:42 -0400

    clean up build script

--- build.sh
+++ build.sh
@@ -2,7 +2,18 @@
 
 set -ex
 
+mkdir -p build/windows-amd64
+mkdir -p build/linux-amd64
+mkdir -p build/linux-arm64
+mkdir -p build/darwin-amd64
+
 GOOS=windows GOARCH=amd64 go build -o build/windows-amd64 ./...
 GOOS=linux GOARCH=amd64 go build -o build/linux-amd64 ./...
 GOOS=linux GOARCH=arm64 go build -o build/linux-arm64 ./...
 GOOS=darwin GOARCH=amd64 go build -o build/darwin-amd64 ./...
+
+cd build
+tar -czf linux-amd64.tar.gz linux-amd64
+tar -czf linux-arm64.tar.gz linux-arm64
+tar -czf darwin-amd64.tar.gz darwin-amd64
+zip -r windows-amd64.zip windows-amd64
